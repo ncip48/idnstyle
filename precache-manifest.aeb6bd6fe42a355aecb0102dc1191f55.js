fa0075fa93979de72d5a9e12ec3964befe211850
ncip48 <mbahcip00@gmail.com>
2020-09-20 14:49:46 +0700

    Updates

---
+++ precache-manifest.aeb6bd6fe42a355aecb0102dc1191f55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1affd884740881f3a2fba51f1034c9db",
+    "url": "/idnstyle/index.html"
+  },
+  {
+    "revision": "301ca53ae8bc1d47f9b5",
+    "url": "/idnstyle/static/css/main.024449a1.chunk.css"
+  },
+  {
+    "revision": "b311f1692932527c4523",
+    "url": "/idnstyle/static/js/2.00bc409f.chunk.js"
+  },
+  {
+    "revision": "6dfd51a424093640fd93ded5a9a98689",
+    "url": "/idnstyle/static/js/2.00bc409f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "301ca53ae8bc1d47f9b5",
+    "url": "/idnstyle/static/js/main.3bb06b97.chunk.js"
+  },
+  {
+    "revision": "353eb95f6210ed22dcf8",
+    "url": "/idnstyle/static/js/runtime-main.53c5ab27.js"
+  }
+]);
